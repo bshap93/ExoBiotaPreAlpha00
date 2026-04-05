55b6260b9e7831862b9d254bb0b406b26277d03f
Ben Shapiro <bshap93@gmail.com>
2026-04-05 15:27:25 -0600

    Guard

--- Assets/Scripts/FirstPersonPlayer/Interactable/BioOrganism/Creatures/OverworldFlier.cs
+++ Assets/Scripts/FirstPersonPlayer/Interactable/BioOrganism/Creatures/OverworldFlier.cs
@@ -3,51 +3,119 @@ using MoreMountains.Feedbacks;
 using NodeCanvas.Framework;
 using UnityEngine;
 using UnityEngine.AI;
-using Random = UnityEngine.Random;
 
 namespace FirstPersonPlayer.Interactable.BioOrganism.Creatures
 {
     [RequireComponent(typeof(Blackboard))]
     [RequireComponent(typeof(AnimancerComponent))]
     [DisallowMultipleComponent]
-    public class OverworldFlierController : CreatureController
+    public class TranslucentFlierController : CreatureController
     {
         [SerializeField] NavMeshAgent navMeshAgent;
+        [SerializeField] protected float movementSpeedThreshold = 0.1f;
+        [Header("Flags")] [SerializeField] protected bool doNotUseIdleState;
+        [Header("Feedbacks")] [SerializeField] protected MMFeedbacks movementLoopFeedbacks;
+
+        [SerializeField] protected float walkRunThreshold = 2f;
+        AnimancerState _moveBackState;
+
+        AnimancerState _rapidMoveState;
 
 
-        [SerializeField] MMFeedbacks creatureCallFeedbacks;
-
-        [SerializeField] float minCallDelay = 10f;
-        [SerializeField] float maxCallDelay = 15f;
+        AnimancerState _slowMoveState;
 
 
-        float _nextCallTime;
-
-
-        protected override void Start()
+        protected override void Awake()
         {
-            base.Start();
-            // Screech immediately on spawn
-            // creatureCallFeedbacks?.PlayFeedbacks();
-            ScheduleNextCall();
+            if (startAsActivated)
+                SetupAnimationStates();
+            else DeactivateCreature();
         }
 
-        void Update()
+        protected virtual void Update()
         {
-            if (!IdleState.IsPlaying)
-                animancerComponent.Play(IdleState);
+            if (!IsActivated) return;
 
-            if (Time.time >= _nextCallTime)
+            var speed = navMeshAgent.velocity.magnitude;
+            var velocity = navMeshAgent.velocity;
+
+            if (speed < movementSpeedThreshold)
             {
-                creatureCallFeedbacks?.PlayFeedbacks();
-                ScheduleNextCall();
+                if (!doNotUseIdleState)
+                    // Idle should NOT interrupt custom animations
+                    if (!IsPlayingCustomAnimation && !IdleState.IsPlaying)
+                        animancerComponent.Play(IdleState, 0.2f);
+
+                movementLoopFeedbacks?.StopFeedbacks();
+            }
+            else
+            {
+                PlayMovementAnimation(velocity, speed);
+                IsPlayingCustomAnimation = false;
+
+                if (movementLoopFeedbacks != null && !movementLoopFeedbacks.IsPlaying)
+                    movementLoopFeedbacks.PlayFeedbacks();
             }
         }
 
-
-        void ScheduleNextCall()
+        public override void ActivateCreature()
         {
-            _nextCallTime = Time.time + Random.Range(minCallDelay, maxCallDelay);
+            base.ActivateCreature();
+            navMeshAgent.enabled = true;
+        }
+
+        public override void DeactivateCreature()
+        {
+            return;
+            base.DeactivateCreature();
+            navMeshAgent.enabled = false;
+        }
+
+        protected virtual void PlayMovementAnimation(Vector3 worldVelocity, float speed)
+        {
+            var animSet = creatureType.animationSet;
+
+            // Non-humanoid: single movement clip, same as before.
+            if (!animSet.IsHumanoid)
+            {
+                if (!MoveState.IsPlaying)
+                    animancerComponent.Play(MoveState, 0.2f);
+
+                return;
+            }
+
+            // Project velocity onto local axes to determine direction.
+            var localVelocity = transform.InverseTransformDirection(worldVelocity);
+            var forwardSpeed = localVelocity.z; // positive = forward, negative = backward
+            var lateralSpeed = localVelocity.x; // positive = right, negative = left
+
+            var isMovingBackward = forwardSpeed < -0.3f;
+
+            if (isMovingBackward && animSet.walkBackAnimation != null)
+            {
+                _moveBackState ??= animancerComponent.States.GetOrCreate(animSet.walkBackAnimation);
+                _moveBackState.Speed = animSet.walkBackAnimationSpeedMultiplier;
+                if (!_moveBackState.IsPlaying) animancerComponent.Play(_moveBackState, 0.2f);
+            }
+
+
+            // Forward movement: walk vs. run.
+            if (speed >= walkRunThreshold && animSet.runActionClip != null)
+            {
+                _rapidMoveState ??= animancerComponent.States.GetOrCreate(animSet.runActionClip.animationClip);
+                _rapidMoveState.Speed = animSet.runAnimationSpeedMultiplier;
+                if (!_rapidMoveState.IsPlaying) animancerComponent.Play(_rapidMoveState, 0.2f);
+            }
+            else
+            {
+                var walkClip = animSet.walkActionClip != null
+                    ? animSet.walkActionClip.animationClip
+                    : animSet.moveAnimation;
+
+                _slowMoveState ??= animancerComponent.States.GetOrCreate(walkClip);
+                _slowMoveState.Speed = animSet.walkAnimationSpeedMultiplier;
+                if (!_slowMoveState.IsPlaying) animancerComponent.Play(_slowMoveState, 0.2f);
+            }
         }
     }
 }
