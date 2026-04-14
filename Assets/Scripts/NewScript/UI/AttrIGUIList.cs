dde26b91aa6af210c612dbe1ec8bb18136110519
Ben Shapiro <bshap93@gmail.com>
2026-04-13 18:25:00 -0600

    Willpower dialogue

--- Assets/Scripts/NewScript/UI/AttrIGUIList.cs
+++ Assets/Scripts/NewScript/UI/AttrIGUIList.cs
@@ -16,6 +16,7 @@ namespace NewScript.UI
         [FormerlySerializedAs("mentalToughnessText")] [SerializeField]
         TMP_Text toughnessText;
         [SerializeField] TMP_Text exobioticText;
+        [SerializeField] TMP_Text willpowerText;
 
 
         void OnEnable()
@@ -37,6 +38,7 @@ namespace NewScript.UI
                 dexterityText.text = eventType.AttributeValues.dexterity.ToString();
                 toughnessText.text = eventType.AttributeValues.toughness.ToString();
                 exobioticText.text = eventType.AttributeValues.exobiotic.ToString();
+                willpowerText.text = eventType.AttributeValues.willpower.ToString();
             }
         }
         public void OnMMEvent(LoadedManagerEvent eventType)
@@ -52,6 +54,7 @@ namespace NewScript.UI
             dexterityText.text = attrMgr.Dexterity.ToString();
             toughnessText.text = attrMgr.Toughness.ToString();
             exobioticText.text = attrMgr.Exobiotic.ToString();
+            willpowerText.text = attrMgr.Willpower.ToString();
         }
     }
 }
